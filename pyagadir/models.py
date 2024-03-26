467dff6ef1311b7133b94f063dded4adb7ec61e2
Martin Engqvist <martin@enginzyme.com>
2024-03-26 11:39:57 +0100

    add docstrings and typing

--- pyagadir/models.py
+++ pyagadir/models.py
@@ -1,15 +1,13 @@
 
 from importlib.resources import files
-import numpy  as np
+import numpy as np
 import pandas as pd
-from typing import List
-import numpy as np
-import numpy as np
+from typing import List, Tuple, Dict
 
 # get params
 datapath = files('pyagadir.data')
 
-table2  = pd.read_csv(
+table2 = pd.read_csv(
     datapath.joinpath('table2.csv'),
     index_col='AA',
 ).astype(float)
@@ -35,142 +33,6 @@ table4b = pd.read_csv(
     datapath.joinpath('table4b.csv'),
     index_col='index',
 ).astype(float)
-
-def get_dG_Int(AA):
-    return table2.loc[AA,'Intrinsic']
-
-dG_Hbond = -0.775
-def get_dG_Hbond(j):
-    return dG_Hbond*max((j-4),0)
-
-def get_dG_i1(AAi,AAi1):
-    charge = 1
-    for AA in [AAi,AAi1]:
-        if AA in set(['R','H','K']): # pos
-            charge *= 1
-        elif AA in set(['D','E']): # neg
-            charge *= -1
-        else:
-            return 0.0
-    else:
-        return 0.05*charge
-
-def get_dG_i3(AAi,AAi3):
-    return table4a.loc[AAi, AAi3]
-
-def get_dG_i4(AAi,AAi4):
-    return table4b.loc[AAi, AAi4]
-
-# seq :: first 4 AA considered for dG_Ncap
-def get_dG_Ncap(seq):
-
-    Ncap = seq[0]
-
-    # possible formation for capping box
-    # when Glu (E) at N+3 (Harper & Rose, 1993;
-    # Dasgupta & Bell, 1993)
-    if seq[3] in ['E','Q','D']:
-        
-        dG_Ncap = table2.loc[Ncap,'Capp_box']
-    
-        # Other capping box options:
-        # Gln (Q) or Asp (D)
-        if seq[3] in ['Q','D'] and dG_Ncap < 0.0:
-            dG_Ncap *= 0.625
-
-        # special capping box case
-        # when there is a Pro (P) at N+1
-        if seq[1] == 'P':
-            dG_Ncap += table2.loc[Ncap,'Pro_N1']
-
-    else:
-        dG_Ncap = table2.loc[Ncap,'N_cap']
-
-    # Asp (D) + 2 special hygroden bond
-    # (Bell et al., 1992; Dasgurpta & Bell, 1993)
-    if seq[2] == 'D':
-        dG_Ncap = min(table2.loc[Ncap,'Asp_2'], dG_Ncap)
-        # only update if more negative
-        # (Pro N+1 is sometimes more stabilizing)
-
-    return dG_Ncap
-
-def get_dG_Ccap(AA):
-    return table2.loc[AA,'C_cap']
-
-def get_dG_dipole(seq):
-
-    N = len(seq)
-    dG_dipole = 0.0
-
-    # N-term contribution (negative)
-    for i in range(0,min(N,10)):
-        AA = seq[i]
-        dG_dipole += table3a.loc[AA,i]
-
-    # C-term contributions (positive)
-    for i in range(-1,-1*min(N,10)-1,-1):
-        AA = seq[i]
-        dG_dipole += table3b.loc[AA,i]
-
-    return dG_dipole
-
-def calc_dG_Hel(i, j, result):
-    # where i is the start and j is the length of helical segment
-
-    dG_Int    = sum(result.int_array[i:i+j])
-
-    # custom case when Pro at N+1
-    if result.seq[i+1] == 'P':
-        dG_Int += (0.66 - 3.33)
-
-    dG_Hbond  = get_dG_Hbond(j)
-    dG_i1_tot = sum(result.i1_array[i:i+j-1])
- 
-    dG_i3_tot = sum(result.i3_array[i:i+j-3])
-    dG_i4_tot = sum(result.i4_array[i:i+j-4])
-    dG_SD     = dG_i1_tot + dG_i3_tot + dG_i4_tot
-
-    dG_Ncap   = result.N_array[i]
-    dG_Ccap   = result.C_array[i+j-1]
-    dG_nonH   = dG_Ncap + dG_Ccap
-    
-    dG_dipole = get_dG_dipole(result.seq[i:i+j])
-
-    dG_Hel = dG_Int + dG_Hbond + dG_SD + dG_nonH + dG_dipole
-
-    dG_dict = {
-        'dG_Helix': dG_Hel,
-        'dG_Int': dG_Int,
-        'dG_Hbond': dG_Hbond,
-        'dG_SD': dG_SD,
-        'dG_nonH': dG_nonH,
-        'dG_dipole': dG_dipole,
-        'dG_i1_tot': dG_i1_tot,
-        'dG_i3_tot': dG_i3_tot,
-        'dG_i4_tot': dG_i4_tot,
-        'dG_Ncap': dG_Ncap,
-        'dG_Ccap': dG_Ccap
-    }
-
-    return dG_Hel, dG_dict
-
-
-def calc_K(dG_Hel: float, T: float = 5.0) -> float:
-    """
-    Calculate the equilibrium constant K.
-
-    Args:
-        dG_Hel (float): The Helix free energy.
-        T (float, optional): Temperature in Celsius. Defaults to 5.0.
-
-    Returns:
-        float: The equilibrium constant K.
-    """
-    R = 1.987204258e-3 # kcal/mol/K
-    celsius_2_kelvin = lambda T: T + 273.0
-    RT = R * celsius_2_kelvin(T)
-    return np.exp(-dG_Hel / RT)
 
 
 class ModelResult(object):
@@ -229,6 +91,212 @@ class ModelResult(object):
         return self.percent_helix
 
 
+def get_dG_Int(AA: str) -> float:
+    """
+    Get the intrinsic free energy contribution.
+
+    Args:
+        AA (str): The amino acid.
+
+    Returns:
+        float: The intrinsic free energy contribution.
+    """
+    return table2.loc[AA, 'Intrinsic']
+
+def get_dG_Hbond(j: int) -> float:
+    """
+    Get the free energy contribution for hydrogen bonding.
+
+    Args:
+        j (int): The index.
+
+    Returns:
+        float: The free energy contribution.
+    """
+    dG_Hbond = -0.775
+    return dG_Hbond * max((j - 4), 0)
+
+def get_dG_i1(AAi: str, AAi1: str) -> float:
+    """
+    Get the free energy contribution for interaction between AAi and AAi1.
+
+    Args:
+        AAi (str): The first amino acid.
+        AAi1 (str): The second amino acid.
+
+    Returns:
+        float: The free energy contribution.
+    """
+    charge = 1
+    for AA in [AAi, AAi1]:
+        if AA in set(['R', 'H', 'K']):
+            charge *= 1
+        elif AA in set(['D', 'E']):
+            charge *= -1
+        else:
+            return 0.0
+    else:
+        return 0.05 * charge
+
+def get_dG_i3(AAi: str, AAi3: str) -> float:
+    """
+    Get the free energy contribution for interaction between AAi and AAi3.
+
+    Args:
+        AAi (str): The first amino acid.
+        AAi3 (str): The third amino acid.
+
+    Returns:
+        float: The free energy contribution.
+    """
+    return table4a.loc[AAi, AAi3]
+
+def get_dG_i4(AAi: str, AAi4: str) -> float:
+    """
+    Get the free energy contribution for interaction between AAi and AAi4.
+
+    Args:
+        AAi (str): The first amino acid.
+        AAi4 (str): The fourth amino acid.
+
+    Returns:
+        float: The free energy contribution.
+    """
+    return table4b.loc[AAi, AAi4]
+
+def get_dG_Ncap(seq: str) -> float:
+    """
+    Get the free energy contribution for N-terminal capping.
+
+    Args:
+        seq (str): The amino acid sequence.
+
+    Returns:
+        float: The free energy contribution.
+    """
+    Ncap = seq[0]
+
+    if seq[3] in ['E', 'Q', 'D']:
+        dG_Ncap = table2.loc[Ncap, 'Capp_box']
+
+        if seq[3] in ['Q', 'D'] and dG_Ncap < 0.0:
+            dG_Ncap *= 0.625
+
+        if seq[1] == 'P':
+            dG_Ncap += table2.loc[Ncap, 'Pro_N1']
+    else:
+        dG_Ncap = table2.loc[Ncap, 'N_cap']
+
+    if seq[2] == 'D':
+        dG_Ncap = min(table2.loc[Ncap, 'Asp_2'], dG_Ncap)
+
+    return dG_Ncap
+
+def get_dG_Ccap(AA: str) -> float:
+    """
+    Get the free energy contribution for C-terminal capping.
+
+    Args:
+        AA (str): The amino acid.
+
+    Returns:
+        float: The free energy contribution.
+    """
+    return table2.loc[AA, 'C_cap']
+
+def get_dG_dipole(seq: str) -> float:
+    """
+    Calculate the dipole free energy contribution.
+
+    Args:
+        seq (str): The amino acid sequence.
+
+    Returns:
+        float: The dipole free energy contribution.
+    """
+    N = len(seq)
+    dG_dipole = 0.0
+
+    for i in range(0, min(N, 10)):
+        AA = seq[i]
+        dG_dipole += table3a.loc[AA, i]
+
+    # C-term contributions (positive)
+    for i in range(-1, -1 * min(N, 10) - 1, -1):
+        AA = seq[i]
+        dG_dipole += table3b.loc[AA, i]
+
+    return dG_dipole
+
+
+def calc_dG_Hel(i: int, j: int, result: ModelResult) -> Tuple[float, Dict[str, float]]:
+    """
+    Calculate the Helix free energy and its components.
+
+    Args:
+        i (int): The start index of the helical segment.
+        j (int): The length of the helical segment.
+        result (ModelResult): The result object.
+
+    Returns:
+        Tuple[float, Dict[str, float]]: The Helix free energy and its components.
+    """
+
+    dG_Int = sum(result.int_array[i:i + j])
+
+    # custom case when Pro at N+1
+    if result.seq[i + 1] == 'P':
+        dG_Int += (0.66 - 3.33)
+
+    dG_Hbond = get_dG_Hbond(j)
+    dG_i1_tot = sum(result.i1_array[i:i + j - 1])
+
+    dG_i3_tot = sum(result.i3_array[i:i + j - 3])
+    dG_i4_tot = sum(result.i4_array[i:i + j - 4])
+    dG_SD = dG_i1_tot + dG_i3_tot + dG_i4_tot
+
+    dG_Ncap = result.N_array[i]
+    dG_Ccap = result.C_array[i + j - 1]
+    dG_nonH = dG_Ncap + dG_Ccap
+
+    dG_dipole = get_dG_dipole(result.seq[i:i + j])
+
+    dG_Hel = dG_Int + dG_Hbond + dG_SD + dG_nonH + dG_dipole
+
+    dG_dict = {
+        'dG_Helix': dG_Hel,
+        'dG_Int': dG_Int,
+        'dG_Hbond': dG_Hbond,
+        'dG_SD': dG_SD,
+        'dG_nonH': dG_nonH,
+        'dG_dipole': dG_dipole,
+        'dG_i1_tot': dG_i1_tot,
+        'dG_i3_tot': dG_i3_tot,
+        'dG_i4_tot': dG_i4_tot,
+        'dG_Ncap': dG_Ncap,
+        'dG_Ccap': dG_Ccap
+    }
+
+    return dG_Hel, dG_dict
+
+
+def calc_K(dG_Hel: float, T: float = 5.0) -> float:
+    """
+    Calculate the equilibrium constant K.
+
+    Args:
+        dG_Hel (float): The Helix free energy.
+        T (float, optional): Temperature in Celsius. Defaults to 5.0.
+
+    Returns:
+        float: The equilibrium constant K.
+    """
+    R = 1.987204258e-3 # kcal/mol/K
+    celsius_2_kelvin = lambda T: T + 273.0
+    RT = R * celsius_2_kelvin(T)
+    return np.exp(-dG_Hel / RT)
+
+
 class AGADIR(object):
     """
     AGADIR class for predicting helical propensity using AGADIR method.
@@ -264,7 +332,7 @@ class AGADIR(object):
         """
         result = self.result
         result.Z_array = 1.0 + result.K_tot_array
-        result.Z       = 1.0 + result.K_tot
+        result.Z = 1.0 + result.K_tot
         result.helical_propensity = self._probability_fxn(result)
         result.percent_helix = np.round(np.mean(result.helical_propensity),3)
 
